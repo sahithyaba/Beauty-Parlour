2afedc58dfb7b05d0acd307b22affb15086cf56c
Mowligha <90541331+Mowligha@users.noreply.github.com>
2022-10-25 18:17:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './components/Main';
+import Events from './components/Events';
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"/>
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
